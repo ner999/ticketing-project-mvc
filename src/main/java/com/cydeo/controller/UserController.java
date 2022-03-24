6a0d8a78b1d2e7d514fa9091c89f036e69b1747a
Ner <Nerijuscep@gmail.com>
2022-03-24 18:40:18 -0500

    part1

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -1,2 +1,17 @@
-package com.cydeo.controller;public class UserController {
+package com.cydeo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+    @GetMapping("/create")
+    public String createUser() {
+
+        return "user/create";
+    }
+
+
 }
--- src/main/java/com/cydeo/entity/BaseEntity.java
+++ src/main/java/com/cydeo/entity/BaseEntity.java
@@ -1,2 +1,19 @@
-package com.cydeo.entity;public class BaseEntity {
+package com.cydeo.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class BaseEntity {
+
+    private Long id;
+    private LocalDateTime insertDateTime;
+    private Long insertUserId;
+    private LocalDateTime lastUpdateTime;
+    private Long lastUpdateUserId;
 }
